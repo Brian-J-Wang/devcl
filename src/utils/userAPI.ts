2e82a3729ef0c663b6af85a13c5621ce95cb4f86
Brian Wang <brianwang7102@gmail.com>
2025-02-04 19:42:39 -0500

    fix: add proper headering content-type

--- src/utils/userAPI.ts
+++ src/utils/userAPI.ts
@@ -3,14 +3,16 @@ interface newUser {
     username: string,
     password: string
 }
-
 export default class UserAPI {
     constructor(private readonly baseUrl:string) {}
 
     //should return information about the 
     addNewUser(newUser: newUser) {
-        return fetch(`${this.baseUrl}/user/signup`, {
+        return fetch(`${this.baseUrl}/users/signup`, {
             method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify({
                 email: newUser.email,
                 username: newUser.username,
@@ -26,16 +28,30 @@ export default class UserAPI {
     }
 
     //should return an authorization token
-    logInUser(username: string, password: string) {
-        return fetch(`${this.baseUrl}/user/signin`, {
-            method: "POST"
+    logInUser(email: string, password: string) {
+        return fetch(`${this.baseUrl}/users/signin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                return Promise.reject();
+            }
         })
     }
 
     getUser(jwt: string) {
-        return fetch(`${this.baseUrl}/user`, {
+        return fetch(`${this.baseUrl}/users`, {
             method: "GET",
             headers: {
+                "Content-Type": "application/json",
                 authorization: `Bearer ${jwt}`
             }
         }).then((res) => {
