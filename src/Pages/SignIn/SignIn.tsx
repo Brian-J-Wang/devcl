cc763ed0dc9d8ac76d837ec6b889673b70aeb9dd
Brian Wang <brianwang7102@gmail.com>
2025-02-04 19:52:39 -0500

    feat: connected login and signup to backend

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,15 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useContext, useState } from "react"
 import { TextButton } from "../../Components/Button/Button"
 import { Container } from "../../Components/Container/Container"
 import Input from "../../Components/Input"
 
 import "./SignIn.css"
+import { UserContext } from "../../Contexts/UserContext"
+
 
 const SignIn: React.FC<{}> = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const userContextConsumer = useContext(UserContext);
+    const navigate = useNavigate();
+
     const handleSubmit = (evt: React.MouseEvent) => {
-        (evt.target as HTMLButtonElement).classList.add("sign-in__submit__state_processing");
+        const element = (evt.target as HTMLButtonElement);
+        element.classList.add("sign-in__submit__state_processing");
 
-
+        userContextConsumer.api.logInUser(email, password).then(() => {
+            element.classList.remove("sign-up__submit__state_processing");
+            element.classList.add("sign-up__submit__state_success");
+            setTimeout(() => {
+                navigate('../collections');
+            }, 1000)
+        })
 
         setTimeout(() => {
             (evt.target as HTMLButtonElement).classList.remove("sign-in__submit__state_processing");
@@ -20,8 +36,8 @@ const SignIn: React.FC<{}> = () => {
         <div className="sign-in">
             <Container className="sign-in__modal">
                 <h2 className="sign-in__header">Sign In</h2>
-                <Input.Text name="Email" type="email"></Input.Text>
-                <Input.Text name="Password" type="password"></Input.Text>
+                <Input.Text name="Email" type="email" stateHandler={setEmail}></Input.Text>
+                <Input.Text name="Password" type="password" stateHandler={setPassword}></Input.Text>
                 <TextButton size="s" radiusStyle="s" style="primary" className="sign-in__submit" onClick={handleSubmit}>Sign In</TextButton>
                 <p className="sign-in__switch"> Need an account? <Link to="../signup" className="sign-in__switch-link">Sign Up</Link></p>
             </Container> 
--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,23 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { TextButton } from "../../Components/Button/Button"
 import { Container } from "../../Components/Container/Container"
 import Input from "../../Components/Input"
 
 import "./SignUp.css"
+import { useContext, useState } from "react"
+import { UserContext } from "../../Contexts/UserContext"
 
 const SignUp: React.FC<{}> = () => {
+    const [email, setEmail] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-    const handleSignUp = () => {
+    const userContextConsumer = useContext(UserContext);
+    const navigate = useNavigate();
 
+    const handleSignUp = (evt: React.MouseEvent) => {
+        const element = (evt.target as HTMLButtonElement)
+        element.classList.add("sign-up__submit__state_processing");
+        userContextConsumer.api.addNewUser(email, username, password).then(() => {
+            element.classList.remove("sign-up__submit__state_processing");
+            element.classList.add("sign-up__submit__state_success");
+            setTimeout(() => {
+                navigate('../signin');
+            }, 1000)
+        }).catch((err) => {
+            console.error(err);
+            (evt.target as HTMLButtonElement).classList.remove("sign-up__submit__state_processing");
+        })
     }
 
     return (
         <div className="sign-up">
             <Container className="sign-up__modal">
                 <h2 className="sign-up__header">Sign Up</h2>
-                <Input.Text name="Email" type="email"></Input.Text>
-                <Input.Text name="Username" type="text"></Input.Text>
-                <Input.Text name="Password" type="password"></Input.Text>
+                <Input.Text name="Email" type="email" stateHandler={setEmail}></Input.Text>
+                <Input.Text name="Username" type="text" stateHandler={setUsername}></Input.Text>
+                <Input.Text name="Password" type="password" stateHandler={setPassword}></Input.Text>
                 <TextButton size="s" radiusStyle="s" style="primary" className="sign-up__submit" onClick={handleSignUp}>Sign Up</TextButton>
                 <p className="sign-up__switch"> Already have an account? <Link to="../signin" className="sign-up__switch-link">Sign In</Link></p>
             </Container> 
