3c3da3e70937ed1ecc1a6460b3a5f19e1cf76ba4
Brian Wang <brianwang7102@gmail.com>
2025-02-10 18:33:57 -0500

    feat: drop down arrow is svg now

--- src/Pages/Collection/CLCategory/Category.tsx
+++ src/Pages/Collection/CLCategory/Category.tsx
@@ -1,6 +1,8 @@
 import { ReactNode, useState} from 'react'
 import "./Category.css"
 
+import arrow from "../../../assets/Arrow.svg";
+
 interface categoryProps {
     id: string,
     name: string,
@@ -9,7 +11,7 @@ interface categoryProps {
 }
 
 const Category : React.FC<categoryProps> = ({ id, name, children, addNewItem}) => {
-    const [isCollapsed, SetIsCollapsed] = useState<Boolean>(false)
+    const [isCollapsed, SetIsCollapsed] = useState<boolean>(false);
 
     const handleKeyboardInput = (evt: any) => {
         if (evt.key == "Enter") {
@@ -22,11 +24,12 @@ const Category : React.FC<categoryProps> = ({ id, name, children, addNewItem}) =
     return (
         <div className="section">
             <div className="section__header">
-                <button className="section__collapse-button" onClick={() => { SetIsCollapsed(!isCollapsed) }}>{isCollapsed ? ">" : "V"}</button>
+                <button className="section__collapse-button" onClick={() => { SetIsCollapsed(!isCollapsed) }}>
+                    <img src={arrow} alt="" className={`${isCollapsed && 'section__drop-down-arrow_collapsed'} section__drop-down-arrow`}/>
+                </button>
                 <h2 className="section__name"> { name } </h2>
             </div>
-            {
-            !isCollapsed && <>
+            <div hidden={isCollapsed}>
                 <div className='section__items'>
                     { children }
                 </div>
@@ -35,8 +38,7 @@ const Category : React.FC<categoryProps> = ({ id, name, children, addNewItem}) =
                     <input type="text" className="section__new-item-input" id={id} placeholder="add something new" 
                         onKeyDown={handleKeyboardInput} onBlur={(evt) => { evt.target.value = "" }} autoComplete='off'/>
                 </div>
-            </>
-            }
+            </div>
         </div>
     )
 }
